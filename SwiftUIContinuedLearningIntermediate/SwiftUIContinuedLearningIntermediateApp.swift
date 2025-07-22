1e26d385b20d204034d51834457e2db6d5319281
Valery-Holovina <valery.holovina09@gmail.com>
2025-07-22 13:03:01 +0300

    Initial Commit

---
+++ SwiftUIContinuedLearningIntermediate/SwiftUIContinuedLearningIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningIntermediateApp.swift
+//  SwiftUIContinuedLearningIntermediate
+//
+//  Created by Valery on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
