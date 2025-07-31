005604ee4323aa4d02b4298c1b1c4d7a93e96a36
Valery-Holovina <valery.holovina09@gmail.com>
2025-07-31 12:08:58 +0300

    Hashable learn

---
+++ SwiftUIContinuedLearningIntermediate/HashableLearn.swift
@@ -0,0 +1,18 @@
+//
+//  HashableLearn.swift
+//  SwiftUIContinuedLearningIntermediate
+//
+//  Created by Valery on 31.07.2025.
+//
+
+import SwiftUI
+
+struct HashableLearn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HashableLearn()
+}
