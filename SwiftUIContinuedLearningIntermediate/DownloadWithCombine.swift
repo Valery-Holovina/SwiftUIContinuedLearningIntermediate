72af04b6d434ec300578b3d8223aa472b02864bd
Valery-Holovina <valery.holovina09@gmail.com>
2025-08-18 11:51:50 +0300

    DownloadWithPublisher example how it will all work

--- SwiftUIContinuedLearningIntermediate/DownloadWithCombine.swift
+++ SwiftUIContinuedLearningIntermediate/DownloadWithCombine.swift
@@ -17,7 +17,29 @@ struct PostModel2: Identifiable, Codable{
 }
 
 @Observable class DownloadWithCombineViewModel{
+    
     var posts: [PostModel2] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts(){
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        
+        // 1. sign up for monthly subscription for package to be delivered
+        // 2. the company would make the package behind the scene
+        // 3. recieve the package at your fron door
+        // 4. make sure box is not damaged
+        // 5. open and make sure the item is correct
+        // 6. use item!
+        // 7. cancellable at any time!
+        
+        
+        
+        
+    }
 }
 
 struct DownloadWithCombine: View {
