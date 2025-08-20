4d58ffb0074cdc07f534fe516e1e199f25459238
Valery-Holovina <valery.holovina09@gmail.com>
2025-08-20 15:24:55 +0300

    TimerAndOnReive simple implementation

--- SwiftUIContinuedLearningIntermediate/TimerAndOnRecive.swift
+++ SwiftUIContinuedLearningIntermediate/TimerAndOnRecive.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct TimerAndOnRecive: View {
+    
+    // autoconnect to start as screen loads
+    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    @State var currentDate: Date = Date()
+    
     var body: some View {
         ZStack{
             RadialGradient(colors: [Color.purple, Color.blue], center: .center, startRadius: 5, endRadius: 500)
                 .ignoresSafeArea()
             
-            Text("Hi")
+            Text(currentDate.description)
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
                 .foregroundStyle(.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
             
             
+        }
+        .onReceive(timer) { value in
+            currentDate = value
         }
     }
 }
