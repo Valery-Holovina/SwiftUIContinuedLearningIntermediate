fd2da4fd478cb52b08574cf424ad983f123492a5
Valery-Holovina <valery.holovina09@gmail.com>
2025-08-20 15:56:27 +0300

    TimerAndOnRecive countdown to date

--- SwiftUIContinuedLearningIntermediate/TimerAndOnRecive.swift
+++ SwiftUIContinuedLearningIntermediate/TimerAndOnRecive.swift
@@ -26,16 +26,33 @@ struct TimerAndOnRecive: View {
     
     
     // Countdown
+    /*
     @State var count: Int = 10
     @State var finishedText: String? = nil
+     */
+    
+    
+    // Countdown to date
+    @State var timeRemaining: String = ""
+    //24 hours after today
+    let futureDate: Date = Calendar.current.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+    
+    func updateTimeRemaining(){
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        timeRemaining = "\(hour):\(minute):\(second)"
+    }
     
     var body: some View {
         ZStack{
             RadialGradient(colors: [Color.purple, Color.blue], center: .center, startRadius: 5, endRadius: 500)
                 .ignoresSafeArea()
             
-//            Text(dateFormator.string(from: currentDate))
-            Text(finishedText ?? "\(count)")
+//            Text(dateFormator.string(from: currentDate))  //Timer
+            /*Text(finishedText ?? "\(count)")*/      //Countdown
+            Text(timeRemaining) // Countdown to date
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
                 .foregroundStyle(.white)
                 .lineLimit(1)
@@ -43,15 +60,23 @@ struct TimerAndOnRecive: View {
             
             
         }
+        //Timer
 //        .onReceive(timer) { value in
 //            currentDate = value
 //        }
+        
+        //Countdown
+//        .onReceive(timer) { _ in
+//            if count<=1{
+//                finishedText = "Wow!"
+//            }else{
+//                count -= 1
+//            }
+//        }
+        
+        // Countdown to date
         .onReceive(timer) { _ in
-            if count<=1{
-                finishedText = "Wow!"
-            }else{
-                count -= 1
-            }
+            updateTimeRemaining()
         }
     }
 }
