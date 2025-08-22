38c9bcab67f94a75b221f40d0037d1e489ed0f0b
Valery-Holovina <valery.holovina09@gmail.com>
2025-08-22 11:28:24 +0300

    FileManager types of directories(Document, Caches, Temporary)

--- SwiftUIContinuedLearningIntermediate/FileManagerLearn.swift
+++ SwiftUIContinuedLearningIntermediate/FileManagerLearn.swift
@@ -7,10 +7,43 @@
 
 import SwiftUI
 
+
+
+class LocalFileManager{
+    
+    static let instance = LocalFileManager()
+    
+    func saveImage(image: UIImage, name: String){
+        
+        guard let data = image.jpegData(compressionQuality: 1.0) else{
+            print("Error getting data")
+            return
+        }
+        
+        // can not be recreated (Document)
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        // can be doenloaded again (Caches)
+        let directory2 = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        // temporary data (Temporary)
+        let directory3 = FileManager.default.temporaryDirectory
+        
+        print(directory)
+        print(directory2)
+        print(directory3)
+        
+//        data.write(to: <#T##URL#>)
+        
+        
+    }
+}
+
+
+
 @Observable class FileManagerViewModel{
     
     var image: UIImage? = nil
     let imageName: String = "car"
+    let manager = LocalFileManager.instance
     
     init() {
         getImageFromAssets()
@@ -18,6 +51,11 @@ import SwiftUI
     
     func getImageFromAssets(){
         image = UIImage(named: imageName)
+    }
+    
+    func saveImage(){
+        guard let image = image else {return}
+        manager.saveImage(image: image, name: imageName)
     }
 }
 
@@ -41,7 +79,7 @@ struct FileManagerLearn: View {
                   
              
                 Button {
-                    
+                    vm.saveImage()
                 } label: {
                     Text("Save to FM")
                         .foregroundStyle(.white)
