0ee79bb2eb33ea0f9a9d79adb36b58ebf6eda20b
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-06 18:10:47 +0300

    if person has reduce transparency in settings

--- SwiftUIContinuedLearningIntermediate/AccessibilityColors.swift
+++ SwiftUIContinuedLearningIntermediate/AccessibilityColors.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
-// XCode -> Open Developer Tool -> Accessebility inspector -> Window -> Show Color Contrast Calculation (should be at least 4.5 - 7 : 1)
+// XCode -> Open Developer Tool -> Accessebility inspector ->
+// Window -> Show Color Contrast Calculation
+// (should be at least 4.5 - 7 : 1)
 
 struct AccessibilityColors: View {
+    
+    // if person has reduce transparency in their settings
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    
+    
+    
     var body: some View {
         VStack{
             Button("Button 1") {
@@ -35,6 +43,9 @@ struct AccessibilityColors: View {
             .buttonStyle(.borderedProminent)
         }
         .font(.largeTitle)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .background( reduceTransparency ?  .black : .black.opacity(0.5))
     }
 }
 
