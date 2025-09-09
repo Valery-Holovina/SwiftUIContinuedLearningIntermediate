60b3547a8c15ee980b3bde292c52ab9d5be2060d
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-09 09:16:20 +0300

    VisualEffect exmp 2

--- SwiftUIContinuedLearningIntermediate/VisualEffectLearn.swift
+++ SwiftUIContinuedLearningIntermediate/VisualEffectLearn.swift
@@ -9,26 +9,49 @@ import SwiftUI
 
 struct VisualEffectLearn: View {
     
-    @State private var showSpacer: Bool = false
+//    @State private var showSpacer: Bool = false
     
     var body: some View {
-        VStack{
-            Text("Hello, World! djdjjdjdjjdjdmdmmddmmdmdmdmdmmdmdmdm")
-                .padding()
-                .background(.red)
-                .visualEffect { content, geometry in
-                    content
-                    //                    .grayscale(geometry.size.width >= 200 ? 1 : 0)
-                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+        
+        ScrollView{
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(.yellow)
+                        .visualEffect { content, geometryProxy in
+                            content
+                                .offset(x: geometryProxy.frame(in: .global).minY * 0.5)
+                        }
                 }
-            if showSpacer{
-                Spacer()
             }
         }
-        .animation(.easeIn, value: showSpacer)
-        .onTapGesture {
-            showSpacer.toggle()
-        }
+        
+        
+        
+        
+        
+        
+        
+        
+//        VStack{
+//            Text("Hello, World! djdjjdjdjjdjdmdmmddmmdmdmdmdmmdmdmdm")
+//                .padding()
+//                .background(.red)
+//                .visualEffect { content, geometry in
+//                    content
+//                    //                    .grayscale(geometry.size.width >= 200 ? 1 : 0)
+//                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+//                }
+//            if showSpacer{
+//                Spacer()
+//            }
+//        }
+//        .animation(.easeIn, value: showSpacer)
+//        .onTapGesture {
+//            showSpacer.toggle()
+//        }
     }
 }
 
